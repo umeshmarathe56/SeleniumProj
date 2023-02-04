7ad88b0c8ec4a632d9030068bca62e7f2f7d00b9
Admin <Admin@DESKTOP-4LB3L2I>
2023-02-04 00:03:51 -0500

    Project

---
+++ src/main/java/genericLab2/Class1.java
@@ -0,0 +1,16 @@
+package genericLab2;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class Class1 {
+	public WebDriver driver;
+	
+	
+	@BeforeMethod
+	public void openall()
+	{
+		
+	}
+
+}
---
+++ src/test/java/scripts/Tc2.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	@Test
+	public void demo() {
+		Reporter.log("hello selenium", true);
+		
+	}
+
+}
